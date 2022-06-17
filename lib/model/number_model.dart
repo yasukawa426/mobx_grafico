464762cf9cf3007f5acede8b7f96fa2ba1649658
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-06-17 11:06:05 -0300

    Adicionado mais um parametro ao model

--- lib/model/number_model.dart
+++ lib/model/number_model.dart
@@ -2,7 +2,8 @@
 class NumberModel {
   ///Conjunto de números aleatórios usados para preencher o gráfico
   final List<int> numbers;
+  ///Conjunto de números aleatórios que imitam anos
+  final List<int> years;
 
-  NumberModel(this.numbers);
-
+  NumberModel(this.numbers, this.years);
 }
