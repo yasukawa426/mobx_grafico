5475bbfe5ba8c3d5e87db4cb0609b2a16271ed21
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-06-17 13:00:16 -0300

    Mudada List para um ObservableList

--- lib/blocs-viewModel/number_controller.dart
+++ lib/blocs-viewModel/number_controller.dart
@@ -8,17 +8,25 @@ class NumberController = NumberControllerBase with _$NumberController;
 
 ///Essa classe faz o controlle dos valores que aparecem no gráfico e no relatório.
 abstract class NumberControllerBase with Store {
-  ///Instância da model criada com uma lista de números vazia.
+  ///Lista Observavel de NumberModel. Começa vazia, depois é preenchida com o método [generateNumbers].
   @observable
-  NumberModel model = NumberModel(List.empty(), List.empty());
+  ObservableList<NumberModel> modelList =
+      ObservableList<NumberModel>.of([NumberModel(0, 0)]);
 
   ///Chama a API [NumberGenerator] para gerar um par de lista de números, então, popula a instância da model com ela.\
   ///[amount] se trata da quantidade de números em cada lista, com valor padrão de 20.
   @action
-  void generateNumbers({amount=20}) {
+  void generateNumbers({amount = 20}) {
     List<int> randomNumbers = NumberGenerator.generateNumbers(amount);
-    List<int> randomYears = NumberGenerator.generateNumbers(amount, min: 2000, max: 30);
-    ///é necessario criar uma nova instancia do objeto para o mobx notificar a interface
-    model = NumberModel(randomNumbers, randomYears);
+    List<int> randomYears =
+        NumberGenerator.generateNumbers(amount, min: 2000, max: 30);
+
+    ///esvazia a lista
+    modelList.clear();
+
+    ///preenche ela
+    for (int i = 0; i < amount; i++) {
+      modelList.add(NumberModel(randomNumbers[i], randomYears[i]));
+    }
   }
 }
--- lib/blocs-viewModel/number_controller.g.dart
+++ lib/blocs-viewModel/number_controller.g.dart
@@ -9,19 +9,19 @@ part of 'number_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$NumberController on NumberControllerBase, Store {
-  late final _$modelAtom =
-      Atom(name: 'NumberControllerBase.model', context: context);
+  late final _$modelListAtom =
+      Atom(name: 'NumberControllerBase.modelList', context: context);
 
   @override
-  NumberModel get model {
-    _$modelAtom.reportRead();
-    return super.model;
+  ObservableList<NumberModel> get modelList {
+    _$modelListAtom.reportRead();
+    return super.modelList;
   }
 
   @override
-  set model(NumberModel value) {
-    _$modelAtom.reportWrite(value, super.model, () {
-      super.model = value;
+  set modelList(ObservableList<NumberModel> value) {
+    _$modelListAtom.reportWrite(value, super.modelList, () {
+      super.modelList = value;
     });
   }
 
@@ -42,7 +42,7 @@ mixin _$NumberController on NumberControllerBase, Store {
   @override
   String toString() {
     return '''
-model: ${model}
+modelList: ${modelList}
     ''';
   }
 }
