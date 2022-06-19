a93c28ede2783b8d06ccb1d32ccbd60928b77778
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-06-19 15:26:21 -0300

    Gridview adicionada e segundo cartao adicionado data-table

--- lib/view/android/android_page.dart
+++ lib/view/android/android_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:mobx_exemplo_grafico/blocs-viewModel/number_controller.dart';
 import 'package:mobx_exemplo_grafico/model/number_model.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:data_table_2/data_table_2.dart';
 
 class AndroidApp extends StatelessWidget {
   const AndroidApp({Key? key}) : super(key: key);
@@ -34,54 +36,87 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Gráfico")),
-      body: Column(children: [
-        const SizedBox(height: 5),
-        const Center(
-            child: Text(
-                "Clique no botão para gerar o seu primeiro conjunto de números. Clique novamente para gerar um novo conjunto.")),
-        const SizedBox(height: 5),
-        ElevatedButton(
-            onPressed: () {
-              setState(() {
-                controller.generateNumbers();
-              });
-            },
-            child: const Text("Gerar números")),
-        Row(
-          children: [
-            Card(child: Observer(
-              builder: (context) {
-                return Column(
-                  children: [
-                    SfCartesianChart(
-
-                        //primaryXAxis: Axis,
-                        title: ChartTitle(text: 'Número por Ano'),
-                        tooltipBehavior: TooltipBehavior(enable: true),
-                        series: <LineSeries>[
-                          LineSeries<NumberModel, int>(
-                              dataSource: controller.modelList,
-                              xValueMapper: (NumberModel valor, _) =>
-                                  valor.year,
-                              yValueMapper: (NumberModel valor, _) =>
-                                  valor.number),
-                        ]),
-                    Text("${controller.modelList[0].number}")
-                  ],
-                );
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const SizedBox(height: 5),
+          const Center(
+              child: Text(
+                  "Clique no botão para gerar o seu primeiro conjunto de números. Clique novamente para gerar um novo conjunto.")),
+          const SizedBox(height: 5),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  controller.generateNumbers();
+                });
               },
-            )),
-            Card(child: Observer(
-              builder: (context) {
-                return Column(children: [
-                  Text("${controller.modelList[0].number}"),
-                  Text("${controller.modelList[0].year}"),
-                ]);
-              },
-            )),
-          ],
-        )
-      ]),
+              child: const Text("Gerar números")),
+          StaggeredGrid.count(
+            crossAxisCount: 1,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            children: [
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Card(
+                    elevation: 5,
+                    child: Observer(
+                      builder: (context) {
+                        return Column(
+                          children: [
+                            SfCartesianChart(
+                                //primaryXAxis: Axis,
+                                title: ChartTitle(text: 'Número por Ano'),
+                                tooltipBehavior: TooltipBehavior(enable: true),
+                                series: <LineSeries>[
+                                  LineSeries<NumberModel, int>(
+                                      dataSource: controller.modelList,
+                                      xValueMapper: (NumberModel valor, _) =>
+                                          valor.year,
+                                      yValueMapper: (NumberModel valor, _) =>
+                                          valor.number),
+                                ]),
+                            Text("${controller.modelList[0].number}")
+                          ],
+                        );
+                      },
+                    )),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Card(
+                    elevation: 5,
+                    child: Observer(
+                      builder: (context) {
+                        return DataTable2(
+                          columnSpacing: 5,
+                          horizontalMargin: 5,
+                          minWidth: MediaQuery.of(context).size.width,
+                          columns: const [
+                            DataColumn2(
+                              label: Text('Valor'),
+                              // size: ColumnSize.L,
+                              numeric: true,
+                            ),
+                            DataColumn(label: Text('Ano'), numeric: true),
+                          ],
+                          rows: List<DataRow>.generate(
+                              controller.modelList.length,
+                              (index) => DataRow(cells: [
+                                    DataCell(Text(
+                                        "${controller.modelList[index].number}")),
+                                    DataCell(Text(
+                                        "${controller.modelList[index].year}")),
+                                  ])),
+                        );
+                      },
+                    )),
+              ),
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
