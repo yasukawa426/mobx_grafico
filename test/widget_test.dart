f12f2c20622cf60d9a957c8bc52dbc57512d888d
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-06-20 13:40:06 -0300

    teste atualizado

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,9 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// // This is a basic Flutter widget test.
+// //
+// // To perform an interaction with a widget in your test, use the WidgetTester
+// // utility in the flutter_test package. For example, you can send tap and scroll
+// // gestures. You can also use WidgetTester to find child widgets in the widget
+// // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
@@ -14,10 +14,10 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
   group("Teste da tela", () {
-    testWidgets('switch', (WidgetTester tester) async {
+    testWidgets('text field', (WidgetTester tester) async {
       await tester.pumpWidget(const AndroidApp());
       //procura o switch pelo tipo, o esperado é achar apenas 1
-      expect(find.byType(Switch), findsOneWidget);
+      expect(find.byType(TextField), findsOneWidget);
     });
 
     testWidgets('gráfico', (WidgetTester tester) async {
