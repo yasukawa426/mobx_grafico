c10c958c0f8dc51cb6420b87062c71b40ae09062
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-06-20 10:44:35 -0300

    imports n usados removidos

--- lib/view/android/android_page.dart
+++ lib/view/android/android_page.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:mobx_exemplo_grafico/blocs-viewModel/number_controller.dart';
