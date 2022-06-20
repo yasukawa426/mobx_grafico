1980e94cd441f8bf92864d259fdc472b39c29357
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-06-20 11:32:30 -0300

    teste simples da interface feito

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,31 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mobx_exemplo_grafico/view/android/android_page.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyHomePage());
+  group("Teste da tela", () {
+    testWidgets('switch', (WidgetTester tester) async {
+      await tester.pumpWidget(const AndroidApp());
+      //procura o switch pelo tipo, o esperado é achar apenas 1
+      expect(find.byType(Switch), findsOneWidget);
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    testWidgets('gráfico', (WidgetTester tester) async {
+      await tester.pumpWidget(const AndroidApp());
+      //procura o chart pelo tipo, o esperado é achar apenas 1
+      expect(find.byType(SfCartesianChart), findsOneWidget);
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    testWidgets('relatório', (WidgetTester tester) async {
+      await tester.pumpWidget(const AndroidApp());
+      //procura o datatable2 pelo tipo, o esperado é achar apenas 1
+      expect(find.byType(DataTable2), findsOneWidget);
+    });
   });
 }
