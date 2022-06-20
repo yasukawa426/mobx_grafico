163486207ef0d451021d39058c94a622b55eafc1
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-06-20 11:06:14 -0300

    teste de gerar numero criado

--- test/controller/number_controller_test.dart
+++ test/controller/number_controller_test.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobx_exemplo_grafico/blocs-viewModel/number_controller.dart';
+import 'package:mobx_exemplo_grafico/model/number_model.dart';
 
 void main() {
   group("showSeries", () {
@@ -20,5 +23,22 @@ void main() {
       controller.changeShowSeries(false);
       expect(controller.showSeries, false);
     });
+  });
+
+  test("Gerador de número", () {
+    NumberController controller = NumberController();
+    Random rng = Random();
+
+    //gera uma lista de 20 numeros e testa pra ver se tem 20
+    int length = 20;
+    controller.generateNumbers();
+    expect(controller.modelList.length, length);
+
+    //testa com uma quantidade de números aleátoria de até 100, 10 vezes
+    for (int i = 0; i < 10; i++) {
+      length = rng.nextInt(100);
+      controller.generateNumbers(amount: length);
+      expect(controller.modelList.length, length);
+    }
   });
 }
