b7d910160494b92328aaaf0969e8e3a0e1b5cc58
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-06-20 10:49:09 -0300

    testes separados em um group

--- test/controller/number_controller_test.dart
+++ test/controller/number_controller_test.dart
@@ -2,17 +2,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mobx_exemplo_grafico/blocs-viewModel/number_controller.dart';
 
 void main() {
-  NumberController controller = NumberController();
-  test("Testando changeShowSeries", () {
-    //o valor esperado padrão é false
-    expect(controller.showSeries, false);
+  group("showSeries", () {
+    NumberController controller = NumberController();
 
-    //muda pra true
-    controller.changeShowSeries(true);
-    expect(controller.showSeries, true);
+    test("valor deveria começar em 'false'", () {
+      expect(controller.showSeries, false);
+    });
 
-    //muda pra false dnv
-    controller.changeShowSeries(false);
-    expect(controller.showSeries, false);
+    test("mudando valor para true", () {
+      //muda pra true
+      controller.changeShowSeries(true);
+      expect(controller.showSeries, true);
+    });
+
+    test("mudando para false", () {
+      //muda pra false dnv
+      controller.changeShowSeries(false);
+      expect(controller.showSeries, false);
+    });
   });
 }
