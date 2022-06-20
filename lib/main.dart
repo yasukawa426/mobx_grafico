a3895061d1505bdf39ed66a547832c853bd7b3b6
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2022-06-19 22:28:35 -0300

    Checa a plataforma e redireciona

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:mobx_exemplo_grafico/view/android/android_page.dart';
-//import 'package:mobx_exemplo_grafico/view/ios/ios_page.dart';
+import 'package:mobx_exemplo_grafico/view/ios/ios_page.dart';
+///Checa a plataforma e redireciona para a tela correta.
 void main() {
-  //Platform.isIOS ? runApp(const IosApp()) : runApp(const AndroidApp());
-  runApp(const AndroidApp());
+  Platform.isIOS ? runApp(const IosApp()) : runApp(const AndroidApp());
+  // runApp(const AndroidApp());
 }
 
 
